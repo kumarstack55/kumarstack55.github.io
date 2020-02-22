a5b0da0525291848db237b17701041eaed7a3779
kumarstack55 <kumarstack55@gmail.com>
2020-02-22 12:46:42 +0900

    変更した

---
+++ render_index.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import jinja2
+import pathlib
+import yaml
+
+
+def render(data, template):
+    script_dir = pathlib.Path(__file__).resolve().parent
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(script_dir))
+    template = env.get_template(template)
+    ctx = dict(data=data)
+    return template.render(ctx)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", default='data.yml')
+    parser.add_argument("--template", default='index.html.j2')
+    parser.add_argument("--output", default='index.html')
+    args = parser.parse_args()
+
+    with open(args.data, encoding='utf-8') as f:
+        data = yaml.safe_load(f)
+
+    html = render(data=data, template=args.template)
+
+    with open(args.output, 'w', encoding='utf-8') as f:
+        f.write(html)
